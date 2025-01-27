71b2434c2900d2659b88a7e10d8e99c98bebca61
Sander Kolman <Sandarr95@users.noreply.github.com>
2025-01-27 12:35:59 +0100

    add env vars for config

--- src/lgtv_idle_sync/__main__.py
+++ src/lgtv_idle_sync/__main__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import asyncio
+import os
 from lgtv_idle_sync.wayland_idle_notifier import WaylandIdleNotifier
 from lgtv_idle_sync.powermanagement_idle_inhibitor import PowerManagementIdleInhibitor
 from lgtv_idle_sync.pulseaudio_notifier import PulseAudioNotifier
@@ -7,9 +8,12 @@ from lgtv_idle_sync import lgtv_idle_client
 
 async def main():
     try:
+        lgtv_screen_idle_time = int(os.environ.get('LGTV_SCREEN_IDLE_TIME', "180"))
+        lgtv_sound_idle_time = int(os.environ.get('LGTV_SOUND_IDLE_TIME', "120"))
+
         loop = asyncio.get_running_loop()
         wayland_idle_notifier = WaylandIdleNotifier(
-            idle_timeout_secs=180,
+            idle_timeout_secs=lgtv_screen_idle_time,
             idle_fn=lgtv_idle_client.idle,
             resume_fn=lgtv_idle_client.resume
         )
@@ -20,7 +24,7 @@ async def main():
 
         pulseaudio_notifier = PulseAudioNotifier(
             resume_audio=resume_audio,
-            min_secs_between_requests=120
+            min_secs_between_requests=lgtv_sound_idle_time
         )
 
         tasks = [
--- src/lgtv_idle_sync/powermanagement_idle_inhibitor.py
+++ src/lgtv_idle_sync/powermanagement_idle_inhibitor.py
@@ -1,7 +1,12 @@
+from importlib import resources
 from dbus_fast.aio import MessageBus
+from dbus_fast.errors import DBusError
+
+import lgtv_idle_sync.resources.dbus
 
 class PowerManagementIdleInhibitor:
 
+    _dbus_resource = 'org.freedesktop.PowerManagement.Inhibit.xml'
     _dbus_bus_name = 'org.freedesktop.PowerManagement.Inhibit'
     _dbus_path = '/org/freedesktop/PowerManagement/Inhibit'
     _dbus_interface = 'org.freedesktop.PowerManagement.Inhibit'
@@ -12,10 +17,9 @@ class PowerManagementIdleInhibitor:
 
     async def connect(self):
         self._message_bus = await MessageBus().connect()
-        # TODO: Only use dynamic introspection for dev, use static file otherwise
-        self._introspection = await self._message_bus.introspect(
-            PowerManagementIdleInhibitor._dbus_bus_name,
-            PowerManagementIdleInhibitor._dbus_path
+        self._introspection = resources.read_text(
+            lgtv_idle_sync.resources.dbus,
+            PowerManagementIdleInhibitor._dbus_resource
         )
         self._power_management = self._message_bus.get_proxy_object(
             PowerManagementIdleInhibitor._dbus_bus_name,
@@ -27,7 +31,10 @@ class PowerManagementIdleInhibitor:
         )
 
         self._policy_agent.on_has_inhibit_changed(self.on_has_inhibit_changed)
-        await self.on_has_inhibit_changed(await self._policy_agent.call_has_inhibit())
+        try:
+          await self.on_has_inhibit_changed(await self._policy_agent.call_has_inhibit())
+        except DBusError:
+          pass
         return self
 
     async def on_has_inhibit_changed(self, has_inhibit):
