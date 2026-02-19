50d3d48ed64ba5e64c33e3b0b33fe6c36406158a
Sander Kolman <Sandarr95@users.noreply.github.com>
2026-02-19 22:06:11 +0100

    more powermanagement improvements, use introspection

--- src/lgtv_idle_sync/powermanagement_idle_inhibitor.py
+++ src/lgtv_idle_sync/powermanagement_idle_inhibitor.py
@@ -4,12 +4,10 @@ from importlib import resources
 from dbus_fast.aio import MessageBus
 from dbus_fast.errors import DBusError
 
-import lgtv_idle_sync.resources.dbus
 from lgtv_idle_sync.interface import Inhibitor
 
 logger = logging.getLogger(__name__)
 
-POWER_MANAGEMENT_DBUS_RESOURCE = 'org.freedesktop.PowerManagement.Inhibit.xml'
 POWER_MANAGEMENT_DBUS_BUS_NAME = 'org.freedesktop.PowerManagement.Inhibit'
 POWER_MANAGEMENT_DBUS_PATH = '/org/freedesktop/PowerManagement/Inhibit'
 POWER_MANAGEMENT_DBUS_INTERFACE = 'org.freedesktop.PowerManagement.Inhibit'
@@ -18,51 +16,52 @@ class PowerManagementIdleInhibitor(Inhibitor):
 
     def __init__(self, idle_manager):
         super().__init__(idle_manager)
+        self._message_bus = None
+        self._power_management_interface = None
 
     async def connect(self):
         self._message_bus = await MessageBus().connect()
-        self._introspection = resources.read_text(
-            lgtv_idle_sync.resources.dbus,
-            POWER_MANAGEMENT_DBUS_RESOURCE
+        _introspection = await self._message_bus.introspect(
+            POWER_MANAGEMENT_DBUS_NAME,
+            POWER_MANAGEMENT_DBUS_PATH
         )
-        self._power_management = self._message_bus.get_proxy_object(
+        _power_management = self._message_bus.get_proxy_object(
             POWER_MANAGEMENT_DBUS_BUS_NAME,
             POWER_MANAGEMENT_DBUS_PATH,
-            self._introspection
+            _introspection
         )
-        self._policy_agent = self._power_management.get_interface(
+        self._power_management_interface = _power_management.get_interface(
             POWER_MANAGEMENT_DBUS_INTERFACE
         )
-
-        self._policy_agent.on_has_inhibit_changed(self._on_has_inhibit_changed)
-        await self._on_has_inhibit_changed(await self._policy_agent.call_has_inhibit())
+        self._power_management_interface.on_has_inhibit_changed(self._on_has_inhibit_changed)
+        await self._on_has_inhibit_changed(await self._power_management_interface.call_has_inhibit())
 
     async def disconnect(self):
-        self._policy_agent.off_has_inhibit_changed(self._on_has_inhibit_changed)
-        self._message_bus.disconnect()
-        await self._message_bus.wait_for_disconnect()
+        if self._power_management_interface:
+            self._power_management_interface.off_has_inhibit_changed(
+                self.on_has_inhibit_changed
+            )
+        if self._message_bus:
+            self._message_bus.disconnect()
+            await self._message_bus.wait_for_disconnect()
+        logger.debug("Disconnected from DBus")
 
     async def run(self):
-        try:
-            while(True):
-                try:
-                    logger.debug("Initiating DBus connection")
-                    await self.connect()
-                    logger.debug("Waiting for DBus connection to close")
-                    await self._message_bus.wait_for_disconnect()
-                except DBusError as e:
-                    logger.error(e)
-                    break
-                except Exception as e:
-                    logger.error(e)
-                    logger.error("DBus disconnected unexpectedly, reconnecting after 5 seconds")
-                    await asyncio.sleep(5)
-                    pass
-        except asyncio.exceptions.CancelledError:
-            raise
-        finally:
-            logger.debug("Disconnecting DBus for shutdown")
-            await self.disconnect()
+        delay = 1
+        max_delay = 30
+        while True:
+            try:
+                await self.connect()
+                delay = 1
+                await self._message_bus.wait_for_disconnect()
+            except asyncio.CancelledError:
+                await self.disconnect()
+                raise
+            except Exception:
+                logger.exception("DBus disconnected")
+                await self.disconnect()
+                await asyncio.sleep(delay)
+                delay = min(delay * 2, max_delay)
 
     async def _on_has_inhibit_changed(self, has_inhibit):
         if has_inhibit:
